3ca81c6777ba92b40b3128f0cffcaa6529753b90
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2024-05-24 00:52:20 +0400

    mariam dopidze
    added html css js file

---
+++ script.js
@@ -0,0 +1,17 @@
+function getAlert(){
+    alert("Mariam Dopidze")
+}
+
+function getClick(number){
+    console.log(number)
+
+    if (number> 38){
+        console.log("მეტია 38-ზე")
+    }
+    else if(number === 38){
+        console.log("tolia 38-ს")
+    }
+    else{
+        console.log("ნაკლებია 38-ზე")
+    }
+}
